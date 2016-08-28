231dc9afd6d83cbeb057adb25bc7fd752852eb80
tomwei7 <tomwei7@163.com>
2016-08-28 13:54:14 +0800

    init

---
+++ README.md
@@ -0,0 +1,46 @@
+# gin-jsonp
+
+JSONP Middleware for Gin Framework
+
+### usage
+
+```
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tomwei7/gin-jsonp"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(jsonp.JsonP())
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run("127.0.0.1:8080") // listen and server on 0.0.0.0:8080
+}
+```
+
+**output:**
+
+```
+curl -v http://127.0.0.1:8080/ping\?callback\=callback
+
+*   Trying 127.0.0.1...
+> GET /ping?callback=callback HTTP/1.1
+> Host: 127.0.0.1:8080
+> User-Agent: curl/7.43.0
+> Accept: */*
+> 
+< HTTP/1.1 200 OK
+< Content-Type: application/json; charset=utf-8
+< Date: Sun, 28 Aug 2016 05:52:00 GMT
+< Content-Length: 28
+< 
+* Connection #0 to host 127.0.0.1 left intact
+callback({"message":"pong"})
+```
+
---
+++ jsonp.go
@@ -0,0 +1,116 @@
+//gin jsonp middleware
+package jsonp
+
+import (
+	"bufio"
+	"bytes"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net"
+)
+
+const (
+	noWritten = -1
+)
+
+type bResponseWriter struct {
+	buffer *bytes.Buffer
+	gin.ResponseWriter
+	size     int
+	isFlush  bool
+	status   int
+	callback string
+}
+
+func (rw *bResponseWriter) WriteHeader(code int) {
+	rw.status = code
+}
+
+func (rw *bResponseWriter) WriteHeaderNow() {
+	rw.ResponseWriter.WriteHeaderNow()
+}
+
+func (rw *bResponseWriter) Write(data []byte) (n int, err error) {
+	n, err = rw.buffer.Write(data)
+	rw.size += n
+	return
+}
+
+func (rw *bResponseWriter) WriteString(s string) (n int, err error) {
+	n, err = io.WriteString(rw.buffer, s)
+	rw.size += n
+	return
+}
+
+func (rw *bResponseWriter) Status() int {
+	return rw.ResponseWriter.Status()
+}
+
+func (rw *bResponseWriter) Size() int {
+	return rw.size
+}
+
+func (rw *bResponseWriter) Written() bool {
+	return rw.size != noWritten
+}
+
+func (rw *bResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	return rw.ResponseWriter.Hijack()
+}
+
+func (rw *bResponseWriter) CloseNotify() <-chan bool {
+	return rw.ResponseWriter.CloseNotify()
+}
+
+func (rw *bResponseWriter) Flush() {
+	if rw.isFlush {
+		return
+	}
+	rw.ResponseWriter.WriteHeader(rw.status)
+	if rw.buffer.Len() > 0 {
+		data := rw.buffer.Bytes()
+		if data[len(data)-1] == 10 {
+			data[len(data)-1] = 41
+		} else {
+			data = append(data, 41)
+		}
+		_, err := rw.ResponseWriter.Write(data)
+		if err != nil {
+			panic(err)
+		}
+		rw.buffer.Reset()
+	}
+	rw.isFlush = true
+}
+
+func (rw *bResponseWriter) start() {
+	rw.buffer.Write([]byte(rw.callback + "("))
+}
+
+var _ gin.ResponseWriter = &bResponseWriter{}
+
+func newbResponseWriter(rw gin.ResponseWriter, callback string) *bResponseWriter {
+	bresp := &bResponseWriter{ResponseWriter: rw, buffer: &bytes.Buffer{}, status: 200, callback: callback}
+	bresp.start()
+	return bresp
+}
+
+func JsonP() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var callback string
+		if jsonp := c.DefaultQuery("jsonp", ""); jsonp != "" {
+			callback = jsonp
+		}
+		if callbackStr := c.DefaultQuery("callback", ""); callbackStr != "" {
+			callback = callbackStr
+		}
+		if callback == "" {
+			c.Next()
+		} else {
+			brw := newbResponseWriter(c.Writer, callback)
+			c.Writer = brw
+			c.Next()
+			brw.Flush()
+		}
+	}
+}
